5435607f82a60821db223c5c297f5566e03abeea
Vasiliy Vazhesov <vasiliy.vazhesov@gmail.com>
2022-04-09 20:19:42 -0700

    push

--- viewer-2/next.config.js
+++ viewer-2/next.config.js
@@ -1,5 +1,5 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
 const config = yaml.load(fs.readFileSync('../config.yml', 'utf8'));
 
