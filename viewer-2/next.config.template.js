10cdcca92ccfeeaad46f9bb71e5b237ed9914e47
Vasiliy Vazhesov <vasiliy.vazhesov@gmail.com>
2022-04-21 11:11:05 -0700

    change config

--- viewer-2/next.config.template.js
+++ viewer-2/next.config.template.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: "${URL}",
-  basePath: "/${BASE_PATH}",
+  basePath: "${BASE_PATH}",
   siteTitle: "${SITE_TITLE}",
   siteDescription: "${SITE_DESCRIPTION}",
   pageSize: 50
